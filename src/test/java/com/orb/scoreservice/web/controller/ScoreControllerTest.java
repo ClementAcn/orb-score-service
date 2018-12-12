bc0a5ba9b180064ee232d5626679fae0f8683d99
Clément Aucoin <c.aucoin@protonmail.com>
2018-12-12 14:42:27 +0100

    Add test file

---
+++ src/test/java/com/orb/scoreservice/web/controller/ScoreControllerTest.java
@@ -0,0 +1,16 @@
+/*package com.orb.scoreservice.web.controller;
+
+
+import com.orb.scoreservice.dao.ScoreDao;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class ScoreControllerTest {
+
+}
+*/
